36ef6b44b77c091042dd0ca6dc754da64e38fd1e
Triton-zzh <939043137@qq.com>
2026-08-16 04:56:27 +0800

    Add files via upload

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,11 +1,9 @@
-package com.example.helloworld
-
+package com.pop.notice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
