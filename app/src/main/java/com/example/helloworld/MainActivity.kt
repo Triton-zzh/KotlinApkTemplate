2dadc31f324e3fed642adc100d2042c4a855c902
Triton-zzh <939043137@qq.com>
2026-08-16 12:34:01 +0800

    Update warning message in MainActivity

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -8,8 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val text = findViewById<TextView>(R.id.textView)
-        text.text = "当前系统已经出现严重故障，强行继续使用可能会导致用户数据丢失！建议停止使用!"
+        text.text = "当前系统已经出现严重故障，强行继续使用可能会导致用户数据丢失！建议"
     }
 }
